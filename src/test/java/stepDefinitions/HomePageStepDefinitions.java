56fb76d0f220de29d45c7009653dcde7ca7e0182
Stephen George <stephen.george@ibm.com>
2021-04-02 21:34:30 -0500

    - continuing scenario - adding dependency injection capability to share webdriver across multiple step definitions

--- src/test/java/stepDefinitions/HomePageStepDefinitions.java
+++ src/test/java/stepDefinitions/HomePageStepDefinitions.java
@@ -4,14 +4,19 @@ import cucumber.api.java.en.Given;
 import org.openqa.selenium.WebDriver;
 import pages.HomePage;
 
-
 public class HomePageStepDefinitions extends BaseTest {
 
     protected WebDriver driver;
 
+    private Injector injector;
+
     private BaseTest baseTest;
 
     private HomePage homePage;
+
+    public HomePageStepDefinitions(Injector injector) {
+        this.injector = injector;
+    }
 
     @Given("user loads amazon home page")
     public void loadHomePage() {
@@ -19,6 +24,7 @@ public class HomePageStepDefinitions extends BaseTest {
         baseTest.initialize();
         driver = baseTest.driver;
 
+        injector.setDriver(driver);
         homePage = new HomePage(driver);
     }
 }
--- src/test/java/stepDefinitions/MainToolBarStepDefinitions.java
+++ src/test/java/stepDefinitions/MainToolBarStepDefinitions.java
@@ -2,17 +2,28 @@ package stepDefinitions;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
 import pages.MainToolBar;
 
 public class MainToolBarStepDefinitions extends BaseTest {
 
     private MainToolBar mainToolBar;
 
-    private BaseTest baseTest;
+//    private BaseTest baseTest;
+
+    private Injector injector;
+
+    private WebDriver driver;
+
+    public MainToolBarStepDefinitions(Injector injector) {
+        this.injector = injector;
+    }
 
     @When("user inputs text into the search bar")
     public void inputTextIntoSearchBar() {
-        driver = baseTest.driver;
+        driver = injector.getDriver();
+
+        mainToolBar = new MainToolBar(driver);
         mainToolBar.inputTextIntoTextBox("qa testing for beginners");
     }
 
